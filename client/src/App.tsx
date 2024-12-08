04ce3d15c19ff840cb14e05c291d41abd9cd63f0
larebear007 <larencozart@gmail.com>
2024-12-08 08:58:23 -0600

    Populate current Todo info to form on click

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -54,9 +54,10 @@ const AddTodoLink = ({ openFormModal }: AddTodoLinkProps) => {
 interface TodoProps {
   todo: Todo;
   openFormModal: () => void;
+  handleTodoClick: (todo: Todo) => void;
 }
 
-const Todo = ( {todo, openFormModal}: TodoProps) => {
+const Todo = ( {todo, openFormModal, handleTodoClick}: TodoProps) => {
   let dueDate;
   if (todo.month && todo.year) {
     dueDate = `${todo.month}/${todo.year.slice(2)}`;
@@ -64,16 +65,20 @@ const Todo = ( {todo, openFormModal}: TodoProps) => {
     dueDate = "No Due Date";
   }
 
+  const openTodoForm = () => {
+    handleTodoClick(todo);
+    openFormModal();
+  }
+
   return (
-    <tr 
-    data-id="{{id}}"
-    onClick={() => openFormModal()}>
-      <td className="list_item">
+    <tr data-id={todo.id}>
+      <td className="list_item"
+          onClick={() => openTodoForm()}>
         {todo.completed 
-         ? <input type="checkbox" name="item_{{id}}" id="item_{{id}}" defaultChecked/>
-         : <input type="checkbox" name="item_{{id}}" id="item_{{id}}"/>}
+         ? <input type="checkbox" name={`item_${todo.id}`} id={`item_${todo.id}`} defaultChecked/>
+         : <input type="checkbox" name={`item_${todo.id}`} id={`item_${todo.id}`}/>}
         <span className="check"></span>
-        <label htmlFor="item_{{id}}">
+        <label htmlFor={`item_${todo.id}`}>
           {todo.title} - {dueDate}
         </label>
       </td>
@@ -86,9 +91,10 @@ const Todo = ( {todo, openFormModal}: TodoProps) => {
 interface TodoListProps {
   todos: Todo[];
   openFormModal: () => void;
+  handleTodoClick: (todo: Todo) => void;
 }
 
-const TodoList = ({ todos, openFormModal }: TodoListProps) => {
+const TodoList = ({ todos, openFormModal, handleTodoClick }: TodoListProps) => {
   return (
     <table cellSpacing="0">
       <tbody>
@@ -97,7 +103,8 @@ const TodoList = ({ todos, openFormModal }: TodoListProps) => {
             <Todo 
               key={todo.id} 
               todo={todo}
-              openFormModal={openFormModal}/>
+              openFormModal={openFormModal}
+              handleTodoClick={handleTodoClick}/>
           );
         })}
       </tbody>
@@ -106,110 +113,213 @@ const TodoList = ({ todos, openFormModal }: TodoListProps) => {
 }
 
 interface FormModalProps {
+  currTodo: Todo | null;
   isOpen: boolean;
   closeFormModal: () => void;
 }
 
-const FormModal = ({ isOpen, closeFormModal }: FormModalProps) => {
+const FormModal = ({ currTodo, isOpen, closeFormModal }: FormModalProps) => {
   if (!isOpen) return null;
 
-  return (
-    <>
-      <div 
-        className="modal" 
-        id="modal_layer"
-        onClick={() => closeFormModal()}></div>
-      <div className="modal" id="form_modal">
-        <form action="" method="post">
-          <fieldset>
-            <ul>
-              <li>
-                <label htmlFor="title">Title</label>
-                <input type="text" name="title" id="title" placeholder="Item 1"/>
-              </li>
-              <li>
-                <label htmlFor="due">Due Date</label>
-                <div className="date">
-                  <select id="due_day" name="due_day">
-                    <option>Day</option>
-                    <option value="01">1</option>
-                    <option value="02">2</option>
-                    <option value="03">3</option>
-                    <option value="04">4</option>
-                    <option value="05">5</option>
-                    <option value="06">6</option>
-                    <option value="07">7</option>
-                    <option value="08">8</option>
-                    <option value="09">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                    <option value="21">21</option>
-                    <option value="22">22</option>
-                    <option value="23">23</option>
-                    <option value="24">24</option>
-                    <option value="25">25</option>
-                    <option value="26">26</option>
-                    <option value="27">27</option>
-                    <option value="28">28</option>
-                    <option value="29">29</option>
-                    <option value="30">30</option>
-                    <option value="31">31</option>
-                  </select>  /
-                  <select id="due_month" name="due_month">
-                    <option>Month</option>
-                    <option value="01">January</option>
-                    <option value="02">February</option>
-                    <option value="03">March</option>
-                    <option value="04">April</option>
-                    <option value="05">May</option>
-                    <option value="06">June</option>
-                    <option value="07">July</option>
-                    <option value="08">August</option>
-                    <option value="09">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                  </select> /
-                  <select id="due_year" name="due_year">
-                    <option>Year</option>
-                    <option>2014</option>
-                    <option>2015</option>
-                    <option>2016</option>
-                    <option>2017</option>
-                    <option>2018</option>
-                    <option>2019</option>
-                    <option>2020</option>
-                    <option>2021</option>
-                    <option>2022</option>
-                    <option>2023</option>
-                    <option>2024</option>
-                    <option>2025</option>
-                  </select>
-                </div>
-              </li>
-              <li>
-                <label htmlFor="description">Description</label>
-                <textarea cols={50} name="description" rows={7} placeholder="Description"></textarea>
-              </li>
-              <li>
-                <input type="submit" value="Save" />
-                <button name="complete">Mark As Complete</button>
-              </li>
-            </ul>
-          </fieldset>
-        </form>
-      </div>
-    </>
-  )
+  if (currTodo) {
+    return (
+      <>
+        <div 
+          className="modal" 
+          id="modal_layer"
+          onClick={() => closeFormModal()}></div>
+        <div className="modal" id="form_modal">
+          <form action="" method="post">
+            <fieldset>
+              <ul>
+                <li>
+                  <label htmlFor="title">Title</label>
+                  <input type="text" name="title" id="title" defaultValue={currTodo.title}/>
+                </li>
+                <li>
+                  <label htmlFor="due">Due Date</label>
+                  <div className="date">
+                    <select id="due_day" name="due_day">
+                      <option>Day</option>
+                      <option value="01">1</option>
+                      <option value="02">2</option>
+                      <option value="03">3</option>
+                      <option value="04">4</option>
+                      <option value="05">5</option>
+                      <option value="06">6</option>
+                      <option value="07">7</option>
+                      <option value="08">8</option>
+                      <option value="09">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option value="20">20</option>
+                      <option value="21">21</option>
+                      <option value="22">22</option>
+                      <option value="23">23</option>
+                      <option value="24">24</option>
+                      <option value="25">25</option>
+                      <option value="26">26</option>
+                      <option value="27">27</option>
+                      <option value="28">28</option>
+                      <option value="29">29</option>
+                      <option value="30">30</option>
+                      <option value="31">31</option>
+                    </select>  /
+                    <select id="due_month" name="due_month">
+                      <option>Month</option>
+                      <option value="01">January</option>
+                      <option value="02">February</option>
+                      <option value="03">March</option>
+                      <option value="04">April</option>
+                      <option value="05">May</option>
+                      <option value="06">June</option>
+                      <option value="07">July</option>
+                      <option value="08">August</option>
+                      <option value="09">September</option>
+                      <option value="10">October</option>
+                      <option value="11">November</option>
+                      <option value="12">December</option>
+                    </select> /
+                    <select id="due_year" name="due_year">
+                      <option>Year</option>
+                      <option>2014</option>
+                      <option>2015</option>
+                      <option>2016</option>
+                      <option>2017</option>
+                      <option>2018</option>
+                      <option>2019</option>
+                      <option>2020</option>
+                      <option>2021</option>
+                      <option>2022</option>
+                      <option>2023</option>
+                      <option>2024</option>
+                      <option>2025</option>
+                    </select>
+                  </div>
+                </li>
+                <li>
+                  <label htmlFor="description">Description</label>
+                  <textarea cols={50} name="description" rows={7}
+                            defaultValue={currTodo.description}></textarea>
+                </li>
+                <li>
+                  <input type="submit" value="Save" />
+                  <button name="complete">Mark As Complete</button>
+                </li>
+              </ul>
+            </fieldset>
+          </form>
+        </div>
+      </>
+    )
+  } else { 
+    return (
+      <>
+        <div 
+          className="modal" 
+          id="modal_layer"
+          onClick={() => closeFormModal()}></div>
+        <div className="modal" id="form_modal">
+          <form action="" method="post">
+            <fieldset>
+              <ul>
+                <li>
+                  <label htmlFor="title">Title</label>
+                  <input type="text" name="title" id="title" placeholder="Item 1"/>
+                </li>
+                <li>
+                  <label htmlFor="due">Due Date</label>
+                  <div className="date">
+                    <select id="due_day" name="due_day">
+                      <option>Day</option>
+                      <option value="01">1</option>
+                      <option value="02">2</option>
+                      <option value="03">3</option>
+                      <option value="04">4</option>
+                      <option value="05">5</option>
+                      <option value="06">6</option>
+                      <option value="07">7</option>
+                      <option value="08">8</option>
+                      <option value="09">9</option>
+                      <option value="10">10</option>
+                      <option value="11">11</option>
+                      <option value="12">12</option>
+                      <option value="13">13</option>
+                      <option value="14">14</option>
+                      <option value="15">15</option>
+                      <option value="16">16</option>
+                      <option value="17">17</option>
+                      <option value="18">18</option>
+                      <option value="19">19</option>
+                      <option value="20">20</option>
+                      <option value="21">21</option>
+                      <option value="22">22</option>
+                      <option value="23">23</option>
+                      <option value="24">24</option>
+                      <option value="25">25</option>
+                      <option value="26">26</option>
+                      <option value="27">27</option>
+                      <option value="28">28</option>
+                      <option value="29">29</option>
+                      <option value="30">30</option>
+                      <option value="31">31</option>
+                    </select>  /
+                    <select id="due_month" name="due_month">
+                      <option>Month</option>
+                      <option value="01">January</option>
+                      <option value="02">February</option>
+                      <option value="03">March</option>
+                      <option value="04">April</option>
+                      <option value="05">May</option>
+                      <option value="06">June</option>
+                      <option value="07">July</option>
+                      <option value="08">August</option>
+                      <option value="09">September</option>
+                      <option value="10">October</option>
+                      <option value="11">November</option>
+                      <option value="12">December</option>
+                    </select> /
+                    <select id="due_year" name="due_year">
+                      <option>Year</option>
+                      <option>2014</option>
+                      <option>2015</option>
+                      <option>2016</option>
+                      <option>2017</option>
+                      <option>2018</option>
+                      <option>2019</option>
+                      <option>2020</option>
+                      <option>2021</option>
+                      <option>2022</option>
+                      <option>2023</option>
+                      <option>2024</option>
+                      <option>2025</option>
+                    </select>
+                  </div>
+                </li>
+                <li>
+                  <label htmlFor="description">Description</label>
+                  <textarea cols={50} name="description" rows={7} placeholder="Description"></textarea>
+                </li>
+                <li>
+                  <input type="submit" value="Save" />
+                  <button name="complete">Mark As Complete</button>
+                </li>
+              </ul>
+            </fieldset>
+          </form>
+        </div>
+      </>
+    )
+  }
+  
 }
 
 interface MainProps {
@@ -218,17 +328,33 @@ interface MainProps {
 
 const Main = ({ todos }: MainProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [currTodo, setCurrTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    console.log("State of currTodo:", currTodo);
+  }, [currTodo]);
 
   const openFormModal = () => setIsOpen(true);
-  const closeFormModal = () => setIsOpen(false);
+  const closeFormModal = () => {
+    setCurrTodo(null);
+    setIsOpen(false);
+  };
+  const handleTodoClick = (todo: Todo) => {
+    setCurrTodo(todo);
+    console.log("Todo I clicked on:", todo);
+  };
+
+
 
   return (
     <main>
       <AddTodoLink openFormModal={openFormModal}/>
       <TodoList 
         todos={todos}
-        openFormModal={openFormModal}/>
-      <FormModal 
+        openFormModal={openFormModal}
+        handleTodoClick={handleTodoClick}/>
+      <FormModal
+        currTodo={currTodo} 
         isOpen={isOpen}
         closeFormModal={closeFormModal}/>
     </main>
